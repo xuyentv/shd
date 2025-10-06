f316f82a83908b7bd86a6e0c5eeaef35cd127b72
Minh X <xuyentv2705@gmail.com>
2025-10-07 01:11:18 +0700

    update 111

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./components/pages/HomePage/HomePage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // không import BrowserRouter
 import AboutPage from "./components/pages/AboutPage/AboutPage";
 import ContactPage from "./components/pages/ContactPage/ContactPage";
 import ReviewPage from "./components/pages/ReviewPage/ReviewPage";
@@ -11,22 +11,20 @@ import NotFoundPage from "./components/pages/NotFoundPage/NotFoundPage";
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <div className="container">
-          <Header />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/reviews" element={<ReviewPage />} />
-            <Route path="/blog" element={<BlogPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-          <Footer />
-        </div>
+    <div className="app">
+      <div className="container">
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/reviews" element={<ReviewPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+        <Footer />
       </div>
-    </Router>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,9 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { BrowserRouter } from "react-router-dom";
 import 'antd/dist/reset.css'
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
