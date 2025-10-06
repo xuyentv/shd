c116b5a51e2c3b58edcb35931182caf93fc05621
Minh X <xuyentv2705@gmail.com>
2025-10-07 00:59:52 +0700

    update code

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,10 @@ import './index.css'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { BrowserRouter } from "react-router-dom";
 import 'antd/dist/reset.css'
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
